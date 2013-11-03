86c3ee158bffee6fee21ebaa71787e741b74be33
Matt Harvey <matt@teamdawg.org>
2013-11-03 11:11:08 -0800

    Minor cleanups

--- reset_db.sh
+++ reset_db.sh
@@ -4,9 +4,4 @@ cd `dirname ${BASH_SOURCE[0]}`
 
 rm -f data/lift.sqlite3
 cat schema.sql import.sql | sqlite3 data/lift.sqlite3
-
-# for FILE in `ls data/*.workout`; do
-# 	cat $FILE | ./workout;
-# done
-
 cat data/*.workout | ./workout
