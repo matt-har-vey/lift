02023dca9a1e9e490a2f5c5df286c46d352bd88c
Matt Harvey <matt@teamdawg.org>
2013-12-30 15:34:48 -0800

    Get the loader working on new C driver

--- Makefile
+++ Makefile
@@ -4,8 +4,8 @@ LEX=flex
 LDLIBS=-ll
 # CC=CC -DPHDEBUG -DSCANDEBUG
 
-MONGO_PATH=/usr/local/include/libmongoc-1.0
-BSON_PATH=/usr/local/include/libbson-1.0
+MONGO_INC=/usr/local/include/libmongoc-1.0
+BSON_INC=/usr/local/include/libbson-1.0
 
 LOAD_OBJ=load.o types.o scan.o parse_handler.o
 
@@ -15,15 +15,15 @@ load_sqlite3: LDLIBS += -lsqlite3
 load_sqlite3: db.o $(LOAD_OBJ)
 	$(CC) $(LDFLAGS) $(LDLIBS) $(LOAD_OBJ) db.o -o load_sqlite3
 
-load_mongo: LDLIBS += -lmongoc-1.0
-load_mongo: LDFLAGS += -L$(MONGO_PATH)
+load_mongo: LDLIBS += -lmongoc-1.0 -lbson-1.0
+load_mongo: LDFLAGS += -L/usr/local/lib
 load_mongo: mongo_db.o $(LOAD_OBJ)
 	$(CC) $(LDFLAGS) $(LDLIBS) $(LOAD_OBJ) mongo_db.o -o load_mongo
 
 dolex: scan.o parse_only.o
 
 mongo_db.o: mongo_db.c
-	$(CC) -I$(MONGO_PATH) -I$(BSON_PATH) mongo_db.c -c mongo_db.c
+	$(CC) -I$(MONGO_INC) -I$(BSON_INC) mongo_db.c -c mongo_db.c
 
 scan.o: scan.l y.tab.c
 y.tab.c: parse.ym
--- mongo_db.c
+++ mongo_db.c
@@ -3,17 +3,19 @@
 #include <mongoc.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define COLL_WORKOUTS "lift.workouts"
-
-mongo conn[1];
+static mongoc_client_t* client;
+static mongoc_collection_t* workouts;
 
 int wk_db_open() {
-	mongo_init(conn);
+    client = 0;
+    workouts = 0;
 
-	int status = mongo_client(conn, "127.0.0.1", 27017);
+    client = mongoc_client_new("mongodb://localhost");
+    workouts = mongoc_client_get_collection(client, "lift", "workouts");
 
-	if (status == MONGO_OK) {
+	if (client != 0 && workouts != 0) {
 		return DB_OK;
 	} else {
 		return DB_EXECUTION;
@@ -21,72 +23,87 @@ int wk_db_open() {
 }
 
 int wk_db_close() {
-	mongo_destroy(conn);
+    mongoc_client_destroy(client);
 	return DB_OK;
 }
 
 int wk_db_insert_workout(wkWorkout* workout) {
+    int res;
+    bson_error_t error;
 	char index_string[100];
-	bson b[1], q[1];
-	bson_init(b);
-	bson_init(q);
+    bson_t *b = bson_new();
+    bson_t *q = bson_new();
+    bson_t *exers = bson_new();
+    bson_t *exer = bson_new();
+    bson_t *sets = bson_new();
+    bson_t *bset = bson_new();
+    bson_t *bdrop = bson_new();
 
 	time_t tt_start = timelocal(&workout->start);
+    bson_uint8_t oid_data[12];
+	memset(oid_data, 0, 12 * sizeof(bson_uint8_t));
+	memcpy(oid_data, &tt_start, sizeof(time_t));
+
 	bson_oid_t oid;
-	memset(&oid.bytes, 0, 12 * sizeof(char));
-	memcpy(&oid.bytes, &tt_start, sizeof(time_t));
+    bson_oid_init_from_data(&oid, oid_data);
 
-	bson_append_oid(q, "_id", &oid);
+	bson_append_oid(q, "_id", 3, &oid);
+	bson_append_oid(b, "_id", 3, &oid);
 
-	bson_append_oid(b, "_id", &oid);
-	bson_append_time_t(b, "start", timelocal(&workout->start));
-	bson_append_time_t(b, "end", timelocal(&workout->end));
-	bson_append_string(b, "mood", workout->mood);
+	bson_append_time_t(b, "start", 5, timelocal(&workout->start));
+	bson_append_time_t(b, "end", 3, timelocal(&workout->end));
+	bson_append_utf8(b, "mood", 4, workout->mood, strlen(workout->mood));
 	if (workout->cardio)
-		bson_append_string(b, "cardio", workout->cardio);
-	bson_append_string(b, "comments", workout->comments);
+		bson_append_utf8(b, "cardio", 6, workout->cardio, strlen(workout->cardio));
+	bson_append_utf8(b, "comments", 8, workout->comments, strlen(workout->comments));
 
-	bson_append_start_array(b, "exercises");
+	bson_append_array_begin(b, "exercises", 9, exers);
 	for (int i = 0; i < workout->num_exercises; i++) {
 		snprintf(index_string, 100, "%d", i);
-		bson_append_start_object(b, index_string);
+		bson_append_document_begin(exers, index_string, strlen(index_string), exer);
 
 		wkExercise* exercise = workout->exercises[i];
 
-		bson_append_string(b, "name", exercise->name);
+		bson_append_utf8(exer, "name", 4, exercise->name, strlen(exercise->name));
 
-		bson_append_start_array(b, "sets");
+		bson_append_array_begin(exer, "sets", 4, sets);
 		for (int j = 0; j < exercise->num_sets; j++) {
 			wkSet* set = exercise->sets[j];
 			snprintf(index_string, 100, "%d", j);
-			bson_append_start_object(b, index_string);
 
-			bson_append_int(b, "reps", set->reps);
-			bson_append_double(b, "weight", set->weight);
+            bson_append_document_begin(sets, index_string, strlen(index_string), bset);
+
+			bson_append_int32(bset, "reps", 4, (bson_int32_t)set->reps);
+			bson_append_double(bset, "weight", 6, set->weight);
 			if (set->extend_reps > 0) {
-				bson_append_start_object(b, "set");
-				bson_append_int(b, "reps", set->extend_reps);
-				bson_append_double(b, "weight", set->extend_weight);
-				bson_append_finish_object(b);
+				bson_append_document_begin(bset, "set", 3, bdrop);
+				bson_append_int32(bdrop, "reps", 4, (bson_int32_t)set->extend_reps);
+				bson_append_double(bdrop, "weight", 6, set->extend_weight);
+				bson_append_document_end(bset, bdrop);
+                bson_reinit(bdrop);
 			}
 			if (set->comment)
-				bson_append_string(b, "comment", set->comment);
+				bson_append_utf8(bset, "comment", 7, set->comment, strlen(set->comment));
 
-			bson_append_finish_object(b);
+            bson_append_document_end(sets, bset);
+            bson_reinit(bset);
 		}
-		bson_append_finish_array(b);
+		bson_append_array_end(exer, sets);
 
-		bson_append_finish_object(b);
+		bson_append_document_end(exers, exer);
+        bson_reinit(exer);
 	}
-	bson_append_finish_array(b);
+	bson_append_array_end(b, exers);
 
-	bson_finish(b);
-	bson_finish(q);
+    res = mongoc_collection_update(workouts, MONGOC_UPDATE_UPSERT, q, b, NULL, &error);
 
-	int res = mongo_update(conn, COLL_WORKOUTS, q, b, MONGO_UPDATE_UPSERT, NULL);
-
-	bson_destroy(b);
+    bson_destroy(bdrop);
+    bson_destroy(bset);
+    bson_destroy(sets);
+    bson_destroy(exer);
+    bson_destroy(exers);
 	bson_destroy(q);
+	bson_destroy(b);
 
-	return res == MONGO_OK ? DB_OK : DB_EXECUTION;
+	return res == 1 ? DB_OK : DB_EXECUTION;
 }
